5082b65aaff1c997ace121a9fe8291513391bf7b
AlanMunozN <amunoz@intersysconsulting.com>
2022-04-20 14:08:34 -0500

    fix font family

--- referral-system/src/App.tsx
+++ referral-system/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.scss';
 import Navbar from "./components/navbar/Navbar";
 
 const App = (props:any) => {
     const {children} = props;
     return (
-        <div className="App">
+        <div>
             <Navbar/>
             {children}
         </div>
